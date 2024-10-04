f32f0a2e7efa85581e7d0128ca86cd8d47cea217
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-04 15:13:55 -0700

    Adds item image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,6 +5,18 @@ const Card = ({ images, name, desc, price, rating, type, ...props }) => {
   return (
     <>
       <div className="cardCont" {...props}>
+        <div
+          style={{
+            backgroundImage: `url(${images[imageIndex % images.length]})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            width: "250px",
+            height: "250px",
+            borderRadius: "15px",
+          }}
+        >
+          </div>
       </div>
     </>
   );
