610f99e5db6c574974da8c019dfba5fc679b6ee5
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-04 15:11:59 -0700

    Creates an empty component for item card

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./Card.css";
+const Card = ({ images, name, desc, price, rating, type, ...props }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+  return (
+    <>
+    </>
+  );
+};
+export default Card;
