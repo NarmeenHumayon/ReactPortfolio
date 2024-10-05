0997ddbbb0f6ee365162f87a3fa0efe3d75c024c
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-04 22:47:37 -0700

    Creates an empty categories components

---
+++ src/components/Categories.jsx
@@ -0,0 +1,10 @@
+import { useRef } from "react";
+import CategorieButton from "./CategorieButton";
+import "./Categories.css";
+const Categories = ({}) => {
+  return (
+    <>
+    </>
+  );
+};
+export default Categories;
