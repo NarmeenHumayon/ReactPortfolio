0b4a1d7b8ea00ce3dfa66c9f9d3bb328620c63b9
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-04 22:51:11 -0700

    Creates a component for categorie button

---
+++ src/components/CategorieButton.jsx
@@ -0,0 +1,9 @@
+import "./CategorieButton.css";
+const CategorieButton = ({ svgPath, title, onButtonClick, ...props }) => {
+  return (
+    <div className="cat-cont" {...props}>
+    </div>
+  );
+};
+
+export default CategorieButton;
