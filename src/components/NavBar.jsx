ca84a6c90ed2caed0f62421b4c85c9d9a1175ce1
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-04 22:48:04 -0700

    Adds categories component to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,27 +2,38 @@ import { useState } from "react";
 import "./NavBar.css";
 import StaysButton from "./StaysButton";
 import ProfileOps from "./Profile";
+import Categories from "./Categories";
 function NavBar() {
   const [currentOpt, setOpt] = useState("stays");
   return (
     <>
       <div className="nav">
-        <img src="airbnb.png" width={70} />
-        <div>
-          <StaysButton
-            text={"Stays"}
-            onbuttonclick={() => setOpt("stays")}
-            isSelected={currentOpt == "stays"}
-          />
-          <StaysButton
-            text={"Experiences"}
-            onbuttonclick={() => setOpt("experiences")}
-            isSelected={currentOpt == "experiences"}
-          />
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <img src="airbnb.png" width={70} />
+          <div>
+            <StaysButton
+              text={"Stays"}
+              onbuttonclick={() => setOpt("stays")}
+              isSelected={currentOpt == "stays"}
+            />
+            <StaysButton
+              text={"Experiences"}
+              onbuttonclick={() => setOpt("experiences")}
+              isSelected={currentOpt == "experiences"}
+            />
+          </div>
+          <div>
+            <ProfileOps />
+          </div>
         </div>
-        <div>
-          <ProfileOps />
-        </div>
+        <Categories />
       </div>
     </>
   );
