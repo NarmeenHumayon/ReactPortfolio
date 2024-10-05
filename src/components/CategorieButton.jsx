fd0fefc26028e1c7df99e5e1eae38b90a59628c1
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-04 22:51:53 -0700

    Adds icon and label to the CategorieButton component

--- src/components/CategorieButton.jsx
+++ src/components/CategorieButton.jsx
@@ -2,6 +2,17 @@ import "./CategorieButton.css";
 const CategorieButton = ({ svgPath, title, onButtonClick, ...props }) => {
   return (
     <div className="cat-cont" {...props}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="currentColor"
+        class="bi bi-cup-hot"
+        viewBox="0 0 16 16"
+      >
+        {svgPath}
+      </svg>
+      {title}
     </div>
   );
 };
