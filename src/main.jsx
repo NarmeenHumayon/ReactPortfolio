a3a59645669c638b6d247d3223707b6d3133a197
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-06 19:07:04 -0700

    Adds admin page to routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import Admin from "./Admin.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route Component={App} path="/"></Route>
+      <Route Component={Admin} path="/admin/"></Route>
+    </Routes>
+  </BrowserRouter>
+);
