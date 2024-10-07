95427a1717a10153cca959435be3b2c2e01fb091
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-06 18:56:18 -0700

    Adds code to test backend

--- src/App.jsx
+++ src/App.jsx
@@ -10,112 +10,120 @@ function App() {
   return (
     <>
       <categoryContext.Provider value={{ category, setCategory }}>
-      <NavBar />
+        <NavBar />
         <div
           className="container"
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-            "https://a0.muscache.com/im/pictures/07824a1a-09a9-4a1d-903c-bcd84b9a6b96.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-        <Card
-          images={[
-            "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-          ]}
-          name={"Card"}
-          rating={4.5}
-          type={"Private room"}
-          desc={"This is a card to show places"}
-          price={"PKR 10"}
-        />
-      </div>
-      <Footer />
+          onClick={() => {
+            fetch("http://localhost:5000/api/test")
+              .then((data) => data.json())
+              .then((res) => {
+                console.log(res);
+              });
+          }}
+        >
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+              "https://a0.muscache.com/im/pictures/07824a1a-09a9-4a1d-903c-bcd84b9a6b96.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+          <Card
+            images={[
+              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
+            ]}
+            name={"Card"}
+            rating={4.5}
+            type={"Private room"}
+            desc={"This is a card to show places"}
+            price={"PKR 10"}
+          />
+        </div>
+        <Footer />
       </categoryContext.Provider>
     </>
   );
