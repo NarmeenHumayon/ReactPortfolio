09977a8c5eca30bc5ec746974be089540b356fae
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-07 12:04:40 -0700

    Makes the categorieButton use category id to identify its "selectedness"

--- src/components/NavBar/Categories/CategorieButton.jsx
+++ src/components/NavBar/Categories/CategorieButton.jsx
@@ -1,16 +1,16 @@
 import { useContext } from "react";
 import "./CategorieButton.css";
 import { categoryContext } from "../../../context/context";
-const CategorieButton = ({ imgsrc, title, onButtonClick, ...props }) => {
+const CategorieButton = ({ imgsrc, title, onButtonClick, id, ...props }) => {
   const value = useContext(categoryContext);
   return (
     <div
       className="cat-cont"
       {...props}
       onClick={() => {
-        value.setCategory(title);
+        value.setCategory(id);
       }}
-      style={value.category == title ? { opacity: 1 } : {}}
+      style={value.category == id ? { opacity: 1 } : {}}
     >
       <img width="20" height="20" src={imgsrc} alt="shop" />
       {title}
