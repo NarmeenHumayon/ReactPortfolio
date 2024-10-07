ae48582a53295664d633d01838568679f72f097e
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-07 12:05:37 -0700

    Adds UI for creating listing items

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,10 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 import "./Admin.css";
 const Admin = () => {
+  const [categories, setCategories] = useState([]);
   const [formData, setFormData] = useState({
     cat_name: "",
     cat_img: null,
   });
+  const [itemFormData, setItemFormData] = useState({
+    item_name: "",
+    item_desc: "",
+    item_price: "",
+    item_cat: "",
+    rating: null,
+    type: "",
+    item_image: null,
+  });
+
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     if (name === "cat_img") {
@@ -13,6 +24,23 @@ const Admin = () => {
       setFormData((prev) => ({ ...prev, [name]: value })); // For text input
     }
   };
+  const handleItemChange = (e) => {
+    const { name, value, files } = e.target;
+    console.log(e.target);
+    if (value == "null") {
+      return;
+    }
+    if (name === "item_image") {
+      setItemFormData((prev) => ({ ...prev, item_image: files[0] })); // For file input
+    } else {
+      setItemFormData((prev) => ({ ...prev, [name]: value })); // For text input
+    }
+  };
+  useEffect(() => {
+    fetch("http://localhost:5000/category")
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <>
       <div className="admin-container">
@@ -71,6 +99,110 @@ const Admin = () => {
               placeholder="enter category name"
               style={{ background: "white", color: "black" }}
             />
+            <input type="submit" placeholder="submit" />
+          </form>
+        </div>
+        <div className="right">
+          <div className="head">Add item</div>
+          <form
+            className="category-form"
+            onSubmit={async (e) => {
+              console.log(itemFormData);
+              e.preventDefault();
+
+              const data = new FormData();
+              data.append("item_name", itemFormData.item_name);
+              data.append("item_desc", itemFormData.item_desc);
+              data.append("item_price", itemFormData.item_price);
+              data.append("cat_id", itemFormData.item_cat);
+              data.append("item_img", itemFormData.item_image);
+              data.append("rating", itemFormData.rating);
+              data.append("type", itemFormData.type);
+
+              try {
+                const response = await fetch("http://localhost:5000/item", {
+                  method: "POST",
+                  body: data,
+                });
+                if (response.ok) {
+                  const result = await response.json();
+                  console.log("Success:", result);
+                } else {
+                  console.error("Upload failed:", response.statusText);
+                }
+              } catch (error) {
+                console.error("Error:", error);
+              }
+            }}
+          >
+            <img
+              src="https://lh5.googleusercontent.com/proxy/t08n2HuxPfw8OpbutGWjekHAgxfPFv-pZZ5_-uTfhEGK8B5Lp-VN4VjrdxKtr8acgJA93S14m9NdELzjafFfy13b68pQ7zzDiAmn4Xg8LvsTw1jogn_7wStYeOx7ojx5h63Gliw"
+              alt=""
+              width={200}
+              height={200}
+              style={{ cursor: "pointer", borderRadius: "5px" }}
+              onClick={() => {
+                document.getElementById("imginp2").click();
+              }}
+            />
+            <input
+              type="file"
+              id="imginp2"
+              name="item_image"
+              accept="image/*"
+              hidden
+              onChange={handleItemChange}
+              style={{ background: "white", color: "black" }}
+              required
+            />
+            <input
+              type="text"
+              name="item_name"
+              value={itemFormData.item_name}
+              onChange={handleItemChange}
+              placeholder="enter name"
+              style={{ background: "white", color: "black" }}
+            />
+            <input
+              type="text"
+              name="item_price"
+              value={itemFormData.item_price}
+              onChange={handleItemChange}
+              placeholder="enter price"
+              style={{ background: "white", color: "black" }}
+            />
+            <input
+              type="text"
+              name="rating"
+              value={itemFormData.rating}
+              onChange={handleItemChange}
+              placeholder="enter rating out of 5"
+              style={{ background: "white", color: "black" }}
+            />
+            <input
+              type="text"
+              name="type"
+              value={itemFormData.type}
+              onChange={handleItemChange}
+              placeholder="enter type"
+              style={{ background: "white", color: "black" }}
+            />
+            <textarea
+              type="text"
+              name="item_desc"
+              value={itemFormData.item_desc}
+              onChange={handleItemChange}
+              placeholder="enter description"
+              style={{ background: "white", color: "black" }}
+            />
+            <select name="item_cat" onChange={handleItemChange}>
+              <option value={"null"}>choose category</option>
+              {categories.map((cat) => (
+                <option key={cat._id} value={cat._id}>
+                  {cat.name}
+                </option>
+              ))}
+            </select>
             <input type="submit" placeholder="submit" />
           </form>
         </div>
