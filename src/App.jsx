7738bc55dd25d917f00948b476514c25acce9692
NarmeenHumayon <bscs22013@itu.edu.pk>
2024-10-07 12:06:38 -0700

    Makes the App.jsx fetch all items and then render them

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import { categoryContext } from "./context/context";
 import NavBar from "./components/NavBar/NavBar";
 import Card from "./components/Card/Card";
 import Footer from "./components/Footer/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [category, setCategory] = useState("");
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    console.log("hello");
+    fetch(`http://localhost:5000/item?cat_id=${category}`)
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, [, category]);
   return (
     <>
       <categoryContext.Provider value={{ category, setCategory }}>
@@ -21,17 +28,17 @@ function App() {
               });
           }}
         >
-          <Card
-            images={[
-              "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
-            ]}
-            name={"Card"}
-            rating={4.5}
-            type={"Private room"}
-            desc={"This is a card to show places"}
-            price={"PKR 10"}
-          />
-          <Card
+          {items.map(({ name, desc, price, imgsrc, rating, type }) => (
+            <Card
+              images={[`http://localhost:5000/uploads/${imgsrc}`]}
+              name={name}
+              rating={`${rating}.0/5`}
+              type={type}
+              desc={desc}
+              price={price}
+            />
+          ))}{" "}
+          {/* <Card
             images={[
               "https://a0.muscache.com/im/pictures/7b43d02d-bc24-4271-b90e-29fcdd43bd56.jpg?im_w=720",
               "https://a0.muscache.com/im/pictures/07824a1a-09a9-4a1d-903c-bcd84b9a6b96.jpg?im_w=720",
@@ -121,7 +128,7 @@ function App() {
             type={"Private room"}
             desc={"This is a card to show places"}
             price={"PKR 10"}
-          />
+          /> */}
         </div>
         <Footer />
       </categoryContext.Provider>
